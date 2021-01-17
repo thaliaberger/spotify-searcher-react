724749064b548bd4d2bdda756f403c094d231c46
Thalía Berger <69408473+thaliaberger@users.noreply.github.com>
2021-01-16 23:39:39 -0300

    Update Artists.js

--- src/components/Artists.js
+++ src/components/Artists.js
@@ -15,7 +15,7 @@ function Artists(props) {
     const fetchData = async () => {
       try {
         const requestToken = btoa(
-          `${"9582ce1bdce64a6388d648f3ae57ab64"}:${"c51adca4ac7d454592fec31b707db6f1"}`
+          `${process.env.REACT_APP_CLIENT_ID}:${process.env.REACT_APP_CLIENT_SECRET}`
         );
 
         const response = await axios.post(
